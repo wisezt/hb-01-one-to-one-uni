0da1398aa1a04662eeae3f40d998d7f3a0457bfa
Ting <wisezt@hotmail.com>
2019-05-15 20:06:18 -0500

    Save the instructor and instructorDetail to DB

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -1,5 +1,7 @@
 package com.luv2code.hibernate.demo;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -14,22 +16,28 @@ public class CreateDemo {
         // create session factory
         SessionFactory theSessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
                 .buildSessionFactory();
 
         // create session
         Session theSession = theSessionFactory.getCurrentSession();
 
         try {
-            //use the session object to save Java object
-            // create a student object
-            Student tempStudent = new Student("Ting", "Zheng", "Ting@gmail.com");
+
+            // Create the object
+            Instructor tempInstructor = new Instructor("Ting001", "Zheng", "Ting@gmail.com");
+            InstructorDetail tempInstructorDetail = new InstructorDetail("www.ting.com", "love you");
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+
+            // associate the object
 
             // start a transaction
             theSession.beginTransaction();
             // save the student object
-            theSession.save(tempStudent);
-            System.out.println("Saving the student ...");
+            theSession.save(tempInstructor);
+            System.out.println("Saving the Instructor ...");
             // commit transaction
             theSession.getTransaction().commit();
 
