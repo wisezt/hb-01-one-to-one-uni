d6875735b3a19a750f6ab3b0df8351446d680eff
Ting <wisezt@hotmail.com>
2019-05-15 20:00:30 -0500

    Create CreateDemo Class

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
     public static void main(String[] args) {
 
